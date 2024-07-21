ab8f6ddc7bca5735ff6d38b0f6fb0b88254f7b7b
acdfxug <yaoyun7@outlook.com>
2024-07-21 20:56:22 +0800

    none

---
+++ LeetCode/连续字符.java
@@ -0,0 +1,20 @@
+package LeetCode;
+
+import java.util.regex.*;
+import static java.lang.Math.*;
+
+public class 连续字符 {
+    static String dup="([a-z])\\1*";
+    static Pattern dp=Pattern.compile(dup);
+    static int maxPower(String s) {
+        int max=0;
+        for(Matcher DUP=dp.matcher(s);DUP.find();){
+            max=max(max,DUP.group().length());
+        }
+        return max;
+    }
+    public static void main(String[] args) {
+        String test="abbcccddddeeeeedcba";
+        System.out.println(maxPower(test));
+    }
+}
