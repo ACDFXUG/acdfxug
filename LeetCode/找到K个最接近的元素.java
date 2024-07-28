aa261eb95ecb094e87ea2c24505af32e87c3cc16
acdfxug <yaoyun7@outlook.com>
2024-07-28 23:11:18 +0800

    最接近的K个元素

---
+++ LeetCode/找到K个最接近的元素.java
@@ -0,0 +1,26 @@
+package LeetCode;
+
+import java.util.*;
+import static java.lang.Math.*;
+
+public class 找到K个最接近的元素 {
+    static List<Integer> findClosestElements(int[] arr, int k, int x) {
+        List<Integer> closest=new ArrayList<Integer>(){{
+            for(int i:arr){
+                add(i);
+            }
+        }};
+        closest.sort((I1,I2)->{
+            int d1=abs(I1-x);
+            int d2=abs(I2-x);
+            return d1==d2?I1-I2:d1-d2;
+        });
+        return new ArrayList<Integer>(closest.subList(0,k)){{
+            sort(null);
+        }};
+    }
+    public static void main(String[] args) {
+        int[] arr={1,2,3,4,5};
+        System.out.println(findClosestElements(arr,4,3));
+    }
+}
