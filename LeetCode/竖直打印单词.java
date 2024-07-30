824e3b6981494abea2b424b500fdd1fe0bb75cb4
acdfxug <yaoyun7@outlook.com>
2024-07-30 16:07:30 +0800

    leetcode

---
+++ LeetCode/竖直打印单词.java
@@ -0,0 +1,33 @@
+package LeetCode;
+
+import java.util.*;
+
+public class 竖直打印单词 {
+    static List<String> printVertically(String s) {
+        String[] strs = s.split(" ");
+        int max = 0;
+        for (String str : strs) {
+            max = Math.max(max, str.length());
+        }
+        List<String> res = new ArrayList<>();
+        for (int i = 0; i < max; i++) {
+            StringBuilder sb = new StringBuilder();
+            for (String str : strs) {
+                if (i < str.length()) {
+                    sb.append(str.charAt(i));
+                } else {
+                    sb.append(" ");
+                }
+            }
+            while (sb.charAt(sb.length() - 1) == ' ') {
+                sb.deleteCharAt(sb.length() - 1);
+            }
+            res.add(sb.toString());
+        }
+        return res;
+    }
+    public static void main(String[] args) {
+        String s="TO BE OR NOT TO BE";
+        printVertically(s).forEach(str->System.out.println(str));
+    }
+}
---
+++ LeetCode/设计一个支持增量操作的栈.java
@@ -0,0 +1,50 @@
+package LeetCode;
+
+import java.util.*;
+
+public class 设计一个支持增量操作的栈 {
+    private static class CustomStack{
+        LinkedList<Integer> stack;
+        final int size;
+        CustomStack(int maxSize){
+            this.size=maxSize;
+            this.stack=new LinkedList<Integer>();
+        }
+        void push(int x) {
+            if(stack.size()<size){
+                stack.add(x);
+            }
+        }
+        int pop() {
+            if(stack.size()==0){
+                return -1;
+            }else{
+                return stack.removeLast();
+            }
+        }
+        void increment(int k, int val) {
+            int min=Math.min(k,stack.size());
+            ListIterator<Integer> it=
+            stack.listIterator();
+            while(min-->0){
+                int temp=it.next();
+                it.set(temp+val);
+            }
+        }
+    }
+    public static void main(String[] args) {
+        CustomStack cs=new CustomStack(3);
+        cs.push(1);
+        cs.push(2);
+        System.out.println(cs.pop());
+        cs.push(2);
+        cs.push(3);
+        cs.push(4);
+        cs.increment(5,100);
+        cs.increment(2,100);
+        System.out.println(cs.pop());
+        System.out.println(cs.pop());
+        System.out.println(cs.pop());
+        System.out.println(cs.pop());
+    }
+}
