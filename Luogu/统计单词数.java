ca2ba3e3c36c634079591e2a890329607ae32aaf
acdfxug <yaoyun7@outlook.com>
2024-08-04 17:51:49 +0800

    无

--- Luogu/统计单词数.java
+++ Luogu/统计单词数.java
@@ -6,7 +6,7 @@ public class 统计单词数 {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         String word=sc.next();
-        String temp=sc.nextLine();
+        // String temp=sc.nextLine();
         String sentence=sc.nextLine();
         String[] words=sentence.split(" ");
         int index=0x7fffffff,ans=0,t=0;
