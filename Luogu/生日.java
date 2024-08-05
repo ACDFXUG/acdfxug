60163ffed3eee4f294471d3c8c2405c75919a9ec
acdfxug <yaoyun7@outlook.com>
2024-08-06 00:37:37 +0800

    数组->Map

--- Luogu/生日.java
+++ Luogu/生日.java
@@ -3,8 +3,7 @@ package Luogu;
 import java.util.*;
 
 public class 生日 {
-    private static record 
-    Birthday(String name,int year,int month,int day) 
+    private static record Birthday(String name,int year,int month,int day) 
     implements Comparable<Birthday> {
         public int compareTo(Birthday other){
             if(year!=other.year){
@@ -20,18 +19,21 @@ public class 生日 {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
-        Birthday[] birthdays=new Birthday[n];
+        Map<Integer,Birthday> rank=new HashMap<>();
         for(int i=0;i<n;i++){
             String name=sc.next();
             int year=sc.nextInt();
             int month=sc.nextInt();
             int day=sc.nextInt();
-            birthdays[i]=new Birthday(name,year,month,day);
+            rank.put(i,new Birthday(name,year,month,day));
         }
-        Arrays.sort(birthdays);
-        for(Birthday birthday:birthdays){
-            System.out.println(birthday.name);
-        }
+        rank.entrySet().stream()
+        .sorted((E1,E2)->{
+            Birthday b1=E1.getValue();
+            Birthday b2=E2.getValue();
+            int cmp=b1.compareTo(b2);
+            return cmp==0?E2.getKey()-E1.getKey():cmp;
+        }).forEach(E->System.out.println(E.getValue().name()));
         sc.close();
     }
 }
