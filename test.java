97343fcc1b8cf2d3d715ecc8e8b83cd9ed34a227
acdfxug <yaoyun7@outlook.com>
2024-08-07 02:55:25 +0800

    打印map

--- test.java
+++ test.java
@@ -1,6 +1,10 @@
+import java.util.*;
 public class test {
     public static void main(String[] args) {
-        int a=10;
-        System.out.println(a);
+        Map<String,Integer> p=new HashMap<>();
+        p.put("a",1);
+        p.put("b",2);
+        p.put("c",3);
+        System.out.println(p);
     }
 }
