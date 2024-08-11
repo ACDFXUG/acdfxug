fc9eb9333ebcf8c66fde70a0a5d3a1b7b052c087
acdfxug <yaoyun7@outlook.com>
2024-08-11 15:25:34 +0800

    luogu

---
+++ Luogu/天下一序数.java
@@ -0,0 +1,17 @@
+package Luogu;
+
+import static java.util.Arrays.sort;
+import static java.lang.String.valueOf;
+
+public class 天下一序数 {
+    public static void main(String[] args) {
+        String[]array=new String[1000];
+        for(int i=0;i<array.length;){
+            array[i]=valueOf(++i);
+        }
+        sort(array);
+        for(String x:array){
+            System.out.println(x);
+        }
+    }
+}
---
+++ Luogu/電卓.java
@@ -0,0 +1,20 @@
+package Luogu;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.*;
+
+public class 電卓 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        BigDecimal A=sc.nextBigDecimal();
+        BigDecimal B=sc.nextBigDecimal();
+        if(!B.equals(BigDecimal.ZERO)){
+            String divide=A.divide(B,3,RoundingMode.HALF_UP).toString();
+            System.out.println(divide.substring(0,divide.indexOf(".")+3));
+        }else{
+            System.out.println("ERROR");
+        }
+        sc.close();
+    }
+}
