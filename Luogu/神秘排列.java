9064c8b573df2278f777f2b73049f52a5e3e73b9
acdfxug <yaoyun7@outlook.com>
2024-08-14 15:42:07 +0800

    luogu

---
+++ Luogu/神秘排列.java
@@ -0,0 +1,24 @@
+package Luogu;
+
+import java.util.Scanner;
+
+public class 神秘排列 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] num=new int[n+1];
+        int[] index=new int[n+1];
+        for(int i=1;i<=n;i++){
+            index[num[i]=sc.nextInt()]=i;
+        }
+        boolean isNotMistery=false;
+        for(int idx:num){
+            if(num[idx]!=index[idx]){
+                isNotMistery=true;
+                break;
+            }
+        }
+        System.out.println(isNotMistery?"NO":"YES");
+        sc.close();
+    }
+}
